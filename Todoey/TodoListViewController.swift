2a338fe9da819b8d4df2500bc10bc4f23de44674
ryandevries920 <ryandevries920@gmail.com>
2024-05-27 23:28:51 +0800

    add UITableViewDelegate method to check or uncheck the item selected, and added the ability to add items to the list.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Find Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Find Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
+    
+    //MARK: - Add new Item
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let addTodo = UIAlertController(title: "Enter ToDo Item", message: nil, preferredStyle: .alert)
+        addTodo.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned addTodo] _ in
+            if let answer = addTodo.textFields![0].text {
+                self.itemArray.append(answer)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+
+        addTodo.addAction(submitAction)
+
+        present(addTodo, animated: true)
+        
+    }
+    
 
 }
 
@@ -35,3 +58,23 @@ extension TodoListViewController {
     }
     
 }
+
+//MARK: - UITableViewDelegate Methods
+
+extension TodoListViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+
+
