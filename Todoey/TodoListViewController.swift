51a71af1ef1e594694ea119700e42519dc95082a
ryandevries920 <ryandevries920@gmail.com>
2024-05-27 21:04:07 +0800

    add hardcoded items to todo list

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Find Eggos", "Destroy Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    // Implement UITableViewDataSource methods here
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 }
 
